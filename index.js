9fc19ca5a0a7ccdee563f731cdc64fbc2e346e46
rakibulislamremon <mdrakibulislamremon@gmail.com>
2024-12-03 18:58:49 +0600

    includes and indexof method

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ var numarray=["11","13","45","76","87","43"];
     numarray.forEach(function(item){
     //console.log(item + "<br>")
 })
+
+//Array includes method
+var Econ=[5,15,4,8,78,55,34];
+    var result= Econ.includes(55);
+//console.log(result)
+
+
+//array Indexof method 
+var Econ=[5,15,4,8,78,55,34];
+    var result= Econ.indexOf(34);
+console.log(result)
